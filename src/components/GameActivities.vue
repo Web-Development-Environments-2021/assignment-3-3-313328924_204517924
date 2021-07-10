a78c7fb9af93bad6c843fdfac20adf236fa600cc
tamirshilony <tamirshilony@gmail.com>
2021-07-10 15:03:45 +0300

    f

--- src/components/GameActivities.vue
+++ src/components/GameActivities.vue
@@ -33,40 +33,6 @@
           </b-tbody>
 
       </b-table-simple>
-    <!-- <b-table striped hover :items="res"></b-table>
-      <table class="table table-bordered">
-          <thead>
-              <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Date</th>
-                  <th scope="col">Time</th>
-                  <th scope="col">Minute</th>
-                  <th scope="col">Event</th>
-                  <th scope="col">Player1</th>
-                  <th scope="col">Player2</th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr v-for="(activity,index) in activities" :key="index">
-                  <th scope="row">{{index +1}}</th>
-                  <td>{{activity.date.substring(0,10)}}</td>
-                  <td>{{activity.time.substring(11,19)}}</td>
-                  <td>{{activity.minute}}</td>
-                  <td><img v-if="activity.event_type === 1" src="../assets/Goal.png"/> 
-                  <img v-if="activity.event_type === 2" src="../assets/Offside.png"/>
-                  <img v-if="activity.event_type === 3" src="../assets/Foul.png"/>
-                  <img v-if="activity.event_type === 4" src="../assets/Red_card.png"/>
-                  <img v-if="activity.event_type === 5" src="../assets/Yellwo_card.png"/>
-                  <img v-if="activity.event_type === 6" src="../assets/Injury.png"/>
-                  <img v-if="activity.event_type === 7" src="../assets/Substitution.png"/>
-                    </td>
-                  <td>{{activity.player_name1}}</td>
-                  <td>{{activity.player_name2}}</td>
-                  
-              </tr>
-          </tbody>
-
-      </table> -->
 </div>
 </template>
 
@@ -89,34 +55,7 @@ export default {
 
 
 }
-// export default {
-//     props: {game: Number},
-//     data(){
-//         return{
-//             activities:[],
-//             res:[],
-//         }
-//     },
 
-//     async created(){
-//         try{
-//             const res = await this.axios.get(`${this.$root.store.domain_server}/leagueManagement/activity/${this.game}`);
-//             this.activities = res.data;
-//             console.log(this.activities);
-//             let items = [];
-//             for(let i = 0;i<this.activities.length;i++){
-//                 items.push({Date:this.activities[i].date.substring(0,10) , Time:this.activities[i].time.substring(11,19) ,Minute:this.activities[i].minute , Event:this.activities[i].event_type , player1:this.activities[i].player_name1 , player2:this.activities[i].player_name2});
-//                 console.log(items);
-//             }
-//             console.log(items);
-//             this.res = items;
-//         }catch(err){
-//             console.log(err);
-//         }
-//     },
-
-
-// }
 </script>
 
 <style>
